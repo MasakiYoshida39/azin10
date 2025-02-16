f37ebad36668a634a360fd669ce23964d95696bb
masaki <masaki@192.168.3.83>
2025-02-16 13:51:29 +0900

    初回

--- src/test/java/com/example/demo/azin03ApplicationTests.java
+++ src/test/java/com/example/demo/azin03ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class motoApplicationTests {
+class azin03ApplicationTests {
 
 	@Test
 	void contextLoads() {
