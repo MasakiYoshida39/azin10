45d0e7448f429db74b2cadff10b245a482a76262
masaki <masaki@192.168.3.83>
2025-02-16 22:01:46 +0900

    リクエストパラメータの違う表現の説明

--- src/main/java/com/example/demo/controller/ParamController.java
+++ src/main/java/com/example/demo/controller/ParamController.java
@@ -12,8 +12,14 @@ public class ParamController {
 	@GetMapping("/get-param")
 	//リクエストをstringに入れる 引数ぎゃくでもOK 
 	public String getParam(@RequestParam String greeting,
+			
+	//パラメータが統一されていない		
 //	public String getParam(@RequestParam(name="g") String greeting,
+			
+	//送られない場合NULLが渡される
 //	public String getParam(@RequestParam(required=false) String greeting,
+			
+	//送られない時(未設定)が引数になる
 //	public String getParam(@RequestParam(defaultValue="(未設定)") String greeting,
 			Model model) {
 	
