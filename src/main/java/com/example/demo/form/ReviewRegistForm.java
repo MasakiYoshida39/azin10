014c16eab393616f9c73d7f582e3b26cc229fb0b
masaki <masaki@192.168.3.82>
2025-02-17 07:02:24 +0900

    from説明追加

--- src/main/java/com/example/demo/form/ReviewRegistForm.java
+++ src/main/java/com/example/demo/form/ReviewRegistForm.java
@@ -3,12 +3,14 @@ package com.example.demo.form;
 import java.sql.Date;
 
 import lombok.Data;
-
+//lombokのデータアノテーションでgetter、setter省略
 @Data
 public class ReviewRegistForm {
 		private Integer restaurantId;
 		private String userId;
+		//来店日
 		private Date visitDate;
+		//評価
 		private Integer rating;
 		private String comment;
 }
