704b6268b0b59057cf341f2534b768626588a881
masaki <masaki@192.168.3.82>
2025-02-17 07:08:37 +0900

    ハンドラーメソッド作成

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -2,10 +2,28 @@ package com.example.demo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.form.ReviewRegistForm;
 //↓コントロールと認識させるためのアノテーション
 @Controller
 public class ReviewController {
+	/*--- レビュー登録画面表示リクエスト ---*/
+	@GetMapping("/show-review-form")
+	public String showReviewForm() {
+		return "regist-review";
+	}
 
+	/*--- レビュー登録リクエスト（登録画面より） ---*/
+	@PostMapping("/regist-review")
+	//formクラスのReviewRegistFormをformに格納
+	public String registReview(@ModelAttribute ReviewRegistForm form) {
+			//tostringが呼び出される
+		System.out.println(form);		// 内容をチェック
+		return "confirm-regist-review";
+	}
 
 	
 }
