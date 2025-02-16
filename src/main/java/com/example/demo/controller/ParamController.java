a03947821c0ac17d7a29b63845543dd313603768
masaki <masaki@192.168.3.82>
2025-02-17 06:29:55 +0900

    コントローラ、ハンドラーメソッド修正

--- src/main/java/com/example/demo/controller/ParamController.java
+++ src/main/java/com/example/demo/controller/ParamController.java
@@ -4,27 +4,25 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 //↓コントロールと認識させるためのアノテーション
 @Controller
 public class ParamController {
-
-	@GetMapping("/get-param")
-	//リクエストをstringに入れる 引数ぎゃくでもOK 
-	public String getParam(@RequestParam String greeting,
-			
-	//パラメータが統一されていない		
-//	public String getParam(@RequestParam(name="g") String greeting,
-			
-	//送られない場合NULLが渡される
-//	public String getParam(@RequestParam(required=false) String greeting,
-			
-	//送られない時(未設定)が引数になる
-//	public String getParam(@RequestParam(defaultValue="(未設定)") String greeting,
-			Model model) {
+	//入力画面遷移
+	@GetMapping("/input")
+	public String input() {
+		return "input";
+	}
 	
+	///post-paramのポストからリクエストを受け取る
+	@PostMapping("/post-param")
+	public String postParam(@RequestParam String greeting,
+			Model model) {
+		//greetingをmGreetingに格納
 		model.addAttribute("mGreeting", greeting);
 		
 		return "display";
 	}
+
 }
