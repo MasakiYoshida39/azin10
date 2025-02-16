53d401069bf4fb7fa9cc59684f56a24fa40f9902
masaki <masaki@192.168.3.83>
2025-02-16 14:00:21 +0900

    HelloSpringControllerに変更

--- src/main/java/com/example/demo/controller/HelloSpringController.java
+++ src/main/java/com/example/demo/controller/HelloSpringController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class test01controller {
+public class HelloSpringController {
 	@GetMapping("/wel")
 	public String wel() {
 		//HTML=test.html
