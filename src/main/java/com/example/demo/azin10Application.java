0f6b40bb007f85c86ec47933699ad2d9017ecf2c
masaki <masaki@192.168.3.82>
2025-02-20 20:50:51 +0900

    アプリケーション名変更

--- src/main/java/com/example/demo/azin10Application.java
+++ src/main/java/com/example/demo/azin10Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class azin09Application {
+public class azin10Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(azin09Application.class, args);
+		SpringApplication.run(azin10Application.class, args);
 	}
 
 }
